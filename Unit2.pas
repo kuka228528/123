d2d88e51b99b3dca5f2c1ba8064ba0118eada451
Trxpl1x <144318653+HunkUmbrella@users.noreply.github.com>
2023-09-12 10:35:12 +0400

    Create Unit2.pas

---
+++ Unit2.pas
@@ -0,0 +1,26 @@
+unit Unit2;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, StdCtrls, jpeg, ExtCtrls;
+
+type
+  TForm2 = class(TForm)
+    Image1: TImage;
+    Label1: TLabel;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form2: TForm2;
+
+implementation
+
+{$R *.dfm}
+
+end.
